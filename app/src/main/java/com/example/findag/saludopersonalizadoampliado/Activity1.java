4a3438073ae664783e35bd4f206617780abf709e
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-13 11:26:01 +0100

    Ejercicio listo para ver.

--- app/src/main/java/com/example/findag/saludopersonalizadoampliado/Activity1.java
+++ app/src/main/java/com/example/findag/saludopersonalizadoampliado/Activity1.java
@@ -1,39 +1,141 @@
 package com.example.findag.saludopersonalizadoampliado;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 
-public class Activity1 extends ActionBarActivity {
+public class Activity1 extends ActionBarActivity
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_activity1);
-    }
+        // Comienza el onCreate
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_activity1);// Cargamos el layout
+
+            // Definimos un boton
+            final Button btnBoton = (Button) findViewById(R.id.b_saludo);// Es necesario castearlo para poder acceder a todos los metodos de button
+            final RadioGroup btnRadio = (RadioGroup) findViewById(R.id.RadioGroup01);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity1, menu);
-        return true;
-    }
+            // Definimos el listener onClick
+            btnBoton.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                    String sr = getResources().getString(R.string.saludoSr);
+                    String sra = getResources().getString(R.string.saludoSra);
+                    TextView textoEntrada = (TextView) findViewById(R.id.entrada);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+                    // MUestra un aviso temporal
+                    if (textoEntrada.getText().toString().equals(""))
+                    {
+                        showToast();
+                        return;
+                    }
+
+                    else
+                    {
+                        TextView textoSalida = (TextView) findViewById(R.id.saludo);
+
+                        // Definimos una variable String que dependiendo del RadioButton marcado contendra sr o sra.
+                        String srSra;
+                        if (btnRadio.getCheckedRadioButtonId() == R.id.rdsr)
+                        {
+                            srSra = sr;
+
+                        }
+                        else
+                        {
+                            srSra= sra;
+
+                        }
+
+                        textoSalida.setText("Buenos dias " + srSra + " " + textoEntrada.getText());
+
+                        // Obtenemos Fecha y Hora
+                        CheckBox timeCheckBox = (CheckBox) findViewById(R.id.checkBox);
+
+                        if (timeCheckBox.isChecked())
+                        {
+                            DatePicker fecha = (DatePicker) findViewById(R.id.datePicker);
+                            String muestraFecha = fecha.getDayOfMonth() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getYear();
+                            TimePicker hora = (TimePicker) findViewById(R.id.timePicker);
+                            muestraFecha += " " + hora.getCurrentHour() + ":" + hora.getCurrentMinute();
+                            textoSalida.setText(" " + srSra + " " + textoEntrada.getText() + " Hoy es " + muestraFecha);
+                        }
+                    }
+
+                }
+            });
+
+            // Mostrar Fechas
+
+            // Instanciamos el checkBox
+            CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox);
+
+            //Definimos listener OnCheckedChange
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+            {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+                {
+                    int visibilidad = isChecked ? View.VISIBLE : View.GONE;
+                    View ver = findViewById(R.id.timePicker);
+                    ver.setVisibility(visibilidad);
+                    ver = findViewById(R.id.datePicker);
+                    ver.setVisibility(visibilidad);
+
+
+                }
+            });
+
+        }
+
+      @Override
+        public boolean onCreateOptionsMenu(Menu menu)
+        {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_activity1, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item)
+        {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+            if (id == R.id.action_settings)
+            {
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+        public void showToast()
+        {
+            Context context = getApplicationContext();
+            int duracion = Toast.LENGTH_SHORT;
+            CharSequence texto = getResources().getString(R.string.noNombre);
+            Toast toast = Toast.makeText(context, texto, duracion);
+            toast.show();
+
+        }
     }
-}
